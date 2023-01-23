37c3630f00f59729fbfb1a636c228e31ef4b4614
Sergey Antonyuk <s.antonyuk@white-soft.ru>
2023-01-23 18:31:49 +1000

    add shared project, next.config.js

--- packages/user/next.config.js
+++ packages/user/next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+const withPlugins = require("next-compose-plugins");
+const withTM = require("next-transpile-modules")(["@monorepo/shared"]);
+const withImages = require("next-images");
+//TODO change config
+module.exports = withPlugins([withTM(), withImages], {
+    webpack: (config) => {
+        // custom webpack config
+        return config;
+    },
+    images: {},
+});
